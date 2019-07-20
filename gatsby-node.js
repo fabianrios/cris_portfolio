799dc30c102a6b90d3a5f2d6fbc24dfcdecbd571
fabianrios <fabian.rios@metals-hub.com>
2019-07-20 12:18:37 +0200

    check intl with key

--- gatsby-node.js
+++ gatsby-node.js
@@ -14,6 +14,7 @@ exports.createPages = ({ actions, graphql }) => {
             id
             fields {
               slug
+              langKey
             }
             frontmatter {
               tags
